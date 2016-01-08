41f985e823f2bf2a159bddd552aeecd5860b3458
siulkilulki <jurkiewiczdawid@gmail.com>
2016-01-08 19:28:50 +0100

    zapomnialem dodac skryptow w index.html wiec dodaje je tym commitem

--- app/angularControllers/ContactCtrl.js
+++ app/angularControllers/ContactCtrl.js
@@ -2,5 +2,27 @@
  * Created by siulkilulki on 08.01.16.
  */
 angular.module('pizzeria').controller('ContactController', function($scope, $state, $stateParams){
+    $scope.cos = 'wysiwetla';
+    /*$http.get('/contact').success(function(data){
+        $scope.jsonContact = data;
 
-});
+    });*/
+} );
+/*
+data.name
+name: 'Pizzeria',
+    address: {
+    street: 'Św. Marcin 1',
+        city: 'Poznań'
+},
+phone: '123-456-789',
+    hours: '12:00 - 22:00'*/
+
+/*
+angular.module('pizzeria').controller('ContactController', ['$http', function($scope, $state, $stateParams, $http){
+    $scope.cos = 'wysiwetla';
+    $http.get('/contact').success(function(data){
+        $scope.jsonContact = data;
+
+    });
+} ]);*/
