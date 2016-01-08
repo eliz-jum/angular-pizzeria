797628eea08a1940806746ada4ea84185d9be27d
siulkilulki <jurkiewiczdawid@gmail.com>
2016-01-08 16:59:46 +0100

    dziala server.js, mozna odpalac za pomoca komendy node server.js

--- app/server.js
+++ app/server.js
@@ -16,8 +16,14 @@ var app = express(),
     orders = {},
     i = 0;
 
-app.use(express.static(__dirname + '/../build/'));
-
+//app.use(express.static(__dirname + '/../build/'));
+app.use(express.static(__dirname + '/'));
+/*
+ Nie wiem czemu poniższe nie zadziałało gdy server.js był w folderze
+ angular-pizzeria/server dlatego przeniosłem server.js do folderu
+ /angular-pizzeria/app
+ app.use(express.static(__dirname + '/../'));
+ */
 app.use(bodyParser.urlencoded({
     extended: false
 }));
