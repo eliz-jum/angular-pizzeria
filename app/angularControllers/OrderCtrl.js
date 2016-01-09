aa1f99926b178c0c4a4d4c3fdac5d7573ccaf464
Zofia Drozd <zegarynka@wp.pl>
2016-01-09 23:25:58 +0100

    replace variables with client object

--- app/angularControllers/OrderCtrl.js
+++ app/angularControllers/OrderCtrl.js
@@ -2,10 +2,7 @@
  * Created by siulkilulki on 06.01.16.
  */
 angular.module('pizzeria').controller('OrderController', function($scope, $state, $stateParams){
-    $scope.firstName = 'andriu';
-    $scope.surname = 'anrzejewski';
-    $scope.phoneNumber = 999888777;
-    $scope.address = 'osiedle kolejowe';
-    $scope.notes = 'bez sosow';
+    $scope.client = {};
+    $scope.notes = '';
     /*$scope.canSubmit = true;*/
 });
