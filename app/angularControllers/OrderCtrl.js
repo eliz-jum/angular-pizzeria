cd7e763a5d1e88f48f6b30666bf7251bbcc68041
Zofia Drozd <zegarynka@wp.pl>
2016-01-11 00:55:32 +0100

    create http post for order; disable Order button on invalid form

--- app/angularControllers/OrderCtrl.js
+++ app/angularControllers/OrderCtrl.js
@@ -1,12 +1,21 @@
 /**
  * Created by siulkilulki on 06.01.16.
  */
-angular.module('pizzeria').controller('OrderController', function($scope, $state, $stateParams){
+angular.module('pizzeria').controller('OrderController', function($scope, $state, $stateParams, $http){
     $scope.client = {};
     $scope.notes = '';
+    $scope.order = [
+    			{
+    				id: 10,
+    				quantity: 5
+    			}];
     $scope.order = function() {
     	if ($scope.client.phoneNumber!=null && $scope.client.address!=null) {
-    		$state.go('status');
+    		$http.post('/order').success(function(data){
+    			data = $scope.order;
+		    }).error(function(data, status) {
+		        console.error('http.get error in OrderCtrl.js', status, data);
+		    });
     	}
     };
 });
