c7ddef6bc1b7a0025c3a44e17a921f70715c4210
siulkilulki <jurkiewiczdawid@gmail.com>
2016-01-11 20:14:14 +0100

    fixed data generation in server, add status code , status works, routing works

--- app/angularControllers/StatusCtrl.js
+++ app/angularControllers/StatusCtrl.js
@@ -1,10 +1,12 @@
 angular.module('pizzeria').controller('StatusController', function($scope, $state, $stateParams, $http){
     $scope.orderId = $stateParams.orderId;
-    $http.get('/order:id').success(function(data){
-        $scope.orderJson = $scope.data;
-        $scope.orderData = $scope.data.order;
-        $scope.ordered = $scope.data.ordered;
-        $scope.estimated = $scope.data.estimated;
+    var id = $scope.orderId
+    $http.get('/order/' + id).success(function(data){
+        //$scope.orderJson = data;
+        //$scope.orderData = data.order;
+        var ordered = data.ordered;
+        var estimated = data.estimated;
+        $scope.orderTime = (estimated - ordered)/60000;
     }).error(function(data, status) {
         console.error('http.get error in StatusCtrl.js', status, data);
     });
