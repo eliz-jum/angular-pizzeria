5244b10154a07d48b263e4d500d48193c9047499
violetcat <zhalia14@gmail.com>
2016-01-11 09:56:26 +0100

    add list of pizzas

--- app/angularControllers/MainCtrl.js
+++ app/angularControllers/MainCtrl.js
@@ -1,3 +1,22 @@
-angular.module('pizzeria').controller('MainController', function($scope, $state, $stateParams){
-
+angular.module('pizzeria').controller('MainController', function($scope, $state, $stateParams, $http){
+    /*$scope.menu=[
+    {
+        id: 1,
+        name: 'Margherita',
+        ingredients: ['Sos', 'Ser'],
+        price: 14.90
+    },
+    {
+        id: 2,
+        name: 'Funghi ',
+        ingredients: ['Sos', 'Ser', 'Pieczarki'],
+        price: 16.90
+    }];*/
+    $scope.menu = [];
+    $http.get('/menu').success(function(data){
+        $scope.menu = data;
+        console.log(data);
+    }).error(function(data, status) {
+        console.error('http.get error in MainCtrl.js', status, data);
+    });
 });
