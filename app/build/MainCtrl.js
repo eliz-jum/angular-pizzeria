f9b9ba1921e2c5fb8c70e69430157b132d060093
siulkilulki <jurkiewiczdawid@gmail.com>
2016-01-13 08:49:32 +0100

    activated gulp

--- app/build/MainCtrl.js
+++ app/build/MainCtrl.js
@@ -1,3 +1,36 @@
-angular.module('pizzeria').controller('MainController', function($scope, $state, $stateParams){
+angular.module('pizzeria').controller('MainController', function($scope, $state, $stateParams, $http, basket){
+    $scope.basketServer = basket.listServer;
+    $scope.basketView = basket.listView;
+    $scope.total = basket.total;
+    $scope.menu = [];
+    
+    $scope.addPizza = function(pizza){
+        basket.add(pizza);
+        $scope.total = basket.sumPrices();
+    };
+    
+    $scope.orderPizza = function(array, pizza) {
+        basket.clearBasket();
+        $scope.addPizza(pizza);
 
+        $state.go('order');
+    };
+
+    $scope.removePizza = function(array, index){
+        array.splice(index, 1);
+        $scope.total = basket.sumPrices();
+    };
+    
+    $scope.updateTotal = function(){
+        console.log("update total");
+        $scope.total = basket.sumPrices();
+    };
+    
+    $http.get('/menu').success(function(data){
+        $scope.menu = data;
+        //console.log(data);
+    }).error(function(data, status) {
+        console.error('http.get error in MainCtrl.js', status, data);        
+    });
+    
 });
--- app/build/StatusCtrl.js
+++ app/build/StatusCtrl.js
@@ -1,3 +1,24 @@
 angular.module('pizzeria').controller('StatusController', function($scope, $state, $stateParams, $http){
-
+    $scope.orderId = $stateParams.orderId;
+    var id = $scope.orderId
+    $http.get('/order/' + id).success(function(data){
+        //$scope.orderJson = data;
+        //$scope.orderData = data.order;
+        var ordered = data.ordered;
+        var estimated = data.estimated;
+        $scope.orderTime = (estimated - ordered)/60000;
+    }).error(function(data, status) {
+        console.error('http.get error in StatusCtrl.js', status, data);
+    });
 });
+/*
+order: req.body.map(function (position) {
+    return {
+        pizza: _.find(menu, {
+            id: position.id
+        }),
+        quantity: position.quantity
+    };
+}),
+    ordered: new Date(now),
+    estimated: new Date(now + _.random(15, 90))*/
