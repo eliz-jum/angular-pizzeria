955ffe713c0a27bd206022c3b11455e0e08494a7
siulkilulki <jurkiewiczdawid@gmail.com>
2016-01-13 10:41:00 +0100

    move build folder to the root of the project, fix gulpfile.js

---
+++ build/services/basketService.js
@@ -0,0 +1,63 @@
+angular.module('pizzeria').factory('basket',function(){
+    var basket = {};
+    basket.listView = [];//{id, name, price quantity}
+    basket.listServer =[];
+    basket.total = 0;
+    
+    var ind;
+    basket.add = function(pizza){
+        var isInListView=false;
+        if (this.listView.length > 0){
+            this.listView.forEach(function(item, index){
+                if (item.id===pizza.id){
+                    isInListView=true;
+                    ind = index;   
+                }
+            });
+            if (isInListView) {
+                console.log("foreach");
+                console.log(ind);
+                console.log("Ta pizza juz byla");
+                basket.listView[ind].quantity+=1;
+            }
+            else {
+                console.log("Tej pizzy nie bylo!");
+                basket.listView.push(pizza);
+            }    
+        }
+        else {
+            console.log("Pusta tablica!");
+            basket.listView.push(pizza);
+        }
+        console.log("view");
+        console.log(basket.listView);
+        
+    };
+    
+    basket.fillListServer = function(){
+        //console.log("basket.listView");
+        //console.log(basket.listView);
+        basket.listView.forEach(function(item){
+            basket.listServer.push({id: item.id, quantity: item.quantity});
+        });
+        return this.listServer;
+    };
+    
+    basket.clearBasket = function(){
+        basket.listView=[];
+        basket.listServer=[];
+        basket.total = 0;
+    };
+    
+    basket.sumPrices = function(){
+        console.log("sumprices");
+        basket.total=0;
+        basket.listView.forEach(function(item){
+            basket.total+=item.price*item.quantity;
+        });
+        basket.total = Math.round(basket.total * 100) / 100;
+        return basket.total;
+    };
+    return basket;
+    
+});
--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,24 @@
  */
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    rimraf = require('gulp-rimraf');
     //concat = require('gulp-concat');
-gulp.task('copy', function() {
-    gulp.src(['./app/js/*','./app/angularControllers/*'])
-    //.pipe(concat('all.js'))
-        .pipe(gulp.dest('./app/build'));
+
+gulp.task('clean', function () {
+    return gulp.src('./build', {read: false})
+        .pipe(rimraf());
+});
+gulp.task('build:dev', ['clean'], function() {
+    return gulp.src(['./app/**'])
+        .pipe(gulp.dest('./build'));
+});
+gulp.task('default', ['build:dev']);
+/*gulp.task('copy', function() {
+    gulp.src(['./app/!**'])
+        .pipe(gulp.dest('./build'));
 });
 gulp.task('watch', function () {
-    gulp.watch('**/*.js', ['copy']);
-});
+    gulp.watch('./app/!**!/!*.*', ['copy']);
+});*/
 
-gulp.task('default', ['copy', 'watch']);
