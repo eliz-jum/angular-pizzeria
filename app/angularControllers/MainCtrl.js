910a7a52c805f78151e1dfaf910cad5f9fff1d06
violetcat <zhalia14@gmail.com>
2016-02-14 19:23:51 +0100

    fix display of ingredients in pizza list

--- app/angularControllers/MainCtrl.js
+++ app/angularControllers/MainCtrl.js
@@ -5,6 +5,9 @@ angular.module('pizzeria').controller('MainController', function($scope, $state,
     $scope.total = basket.total;
     $scope.menu = [];
     
+    var ingredients = [];
+    var extraIngredients = [];
+    
     $scope.addPizza = function(pizza){
         basket.add(pizza);
         $scope.total = basket.sumPrices();
@@ -27,10 +30,22 @@ angular.module('pizzeria').controller('MainController', function($scope, $state,
         $scope.total = basket.sumPrices();
     };
     
-    $scope.rcustomisePizza = function(array, index){
+    $scope.customisePizza = function(array, index){
         //array to tablica skladnikoww
         $scope.total = basket.sumPrices();//trzeba bedzie zmienic sumPrices zeby dodawalo ceny za dodatkowe skladniki
     };
+    $scope.showIngredients = function(ingredientsNumbers){
+        var result = [];
+        ingredientsNumbers.forEach(function(item) {
+            ingredients.forEach(function(item2) {
+                if (item == item2.id)
+                    result.push(item2.label);
+            });
+        });
+        return result;
+        
+    }
+    
     
     $http.get('/menu').success(function(data){
         $scope.menu = data;
@@ -38,4 +53,19 @@ angular.module('pizzeria').controller('MainController', function($scope, $state,
     }).error(function(data, status) {
         console.error('http.get error in MainCtrl.js', status, data);        
     });
+    
+    $http.get('/ingredients').success(function(data){
+        ingredients = data;
+        //console.log(data);
+    }).error(function(data, status) {
+        console.error('http.get error in MainCtrl.js', status, data);        
+    });
+    
+    $http.get('/extras').success(function(data){
+        extraIngredients = data;
+        //console.log(data);
+    }).error(function(data, status) {
+        console.error('http.get error in MainCtrl.js', status, data);        
+    });
+    
 });
